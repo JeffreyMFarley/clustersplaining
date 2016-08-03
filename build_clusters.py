b7a19d8c5efc4153520cb017069cfcc80bad7517
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2016-08-03 12:25:14 -0400

    Separate build and display logic

--- build_clusters.py
+++ build_clusters.py
@@ -1,100 +1,71 @@
 from swatch import Swatch
-from page import Page
-from operator import attrgetter
-from hew import KMeans, KDTree
-from hew.structures.vector import distance_euclid_squared
-import argparse
-
-
-class Clusters(Page):
-    def __init__(self, **kwargs):
-        self.k = kwargs['k']
-        self.nearest = kwargs['nearest']
-        self.hsl = kwargs['hsl']
-        self.steps = kwargs['steps']
-
-        if self.hsl:
-            self.valueAttrs = ['hue', 'saturation', 'light']
-        else:
-            self.valueAttrs = ['red', 'green', 'blue']
-
-    def _toRgb(self, vector):
-        import colorsys
-
-        if self.hsl:
-            h = vector[0] / 100
-            s = vector[1] / 100
-            v = 256 * vector[2] / 100
-            vector = colorsys.hsv_to_rgb(h, s, v)
-
-        return [int(vector[0]), int(vector[1]), int(vector[2])]
-
-    def onCanvas(self):
-        for i, members in enumerate(self.clusters.groups):
-            rgb = self.palette[i].rgb
-            for member in members:
-                sw = Swatch(self._toRgb(member))
-                self.addClustered(rgb, sw)
-
-    def onLegend(self):
-        for sw in sorted(self.palette, key=attrgetter(*self.valueAttrs)):
-            self.addLegend(sw.name, sw.rgb)
-
-    def calculate(self):
-        distance_fn = distance_euclid_squared
-        swatches = list(Swatch.acquire())
-        ranged = self.valueAttrs
-        labels = ['name']
-
-        print('Calculating {0} clusters'.format(self.k))
-        self.clusters = KMeans.fromTable(self.k, swatches, ranged, distance_fn)
-
-        # Convert cluster centroids to palette swatches
-        self.palette = []
-        if self.nearest:
-            print('Building nearest neighbor tree')
-            tree = KDTree.fromTable(swatches, ranged, labels)
-
-            for centroid in self.clusters.MU:
-                rgb, name, _ = tree.nearest_neighbor(centroid)
-                colors = self._toRgb(rgb)
-                self.palette.append(Swatch(colors, name[0]))
-
-        else:
-            for centroid in self.clusters.MU:
-                colors = self._toRgb(centroid)
-                name = '#{0:02x}{1:02x}{2:02x}'.format(*colors)
-                self.palette.append(Swatch(colors, name))
 
 # -----------------------------------------------------------------------------
-# Main
+# vector -> vector
 # -----------------------------------------------------------------------------
 
 
-def buildArgParser():
-    description = 'Demonstrate k-Means using Behr colors'
-    p = argparse.ArgumentParser(description=description)
-    p.add_argument('-k', '--clusters',
-                   type=int, dest='k', default=8,
-                   help='the number of clusters to create (default: 8)')
-    p.add_argument('-n', '--use-nearest',
-                   action='store_true', dest='nearest', default=False,
-                   help='cluster colors come from the input set')
-    p.add_argument('-s', '--show-steps',
-                   action='store_true', dest='steps', default=False,
-                   help='indicate the steps in the cluster calculation')
-    p.add_argument('--use-hsl',
-                   action='store_true', dest='hsl', default=False,
-                   help='use HSL not RGB for cluster calc')
-    p.add_argument('--trial',
-                   metavar='NAME', dest='trial', required=False,
-                   help='identify a cluster run')
+def quantize(vector):
+    return [int(x) for x in vector]
 
-    return p
+
+def toRgb(vector):
+    import colorsys
+
+    h = vector[0] / 100
+    s = vector[1] / 100
+    v = 256 * vector[2] / 100
+    return quantize(colorsys.hsv_to_rgb(h, s, v))
+
+# -----------------------------------------------------------------------------
+# vector[] -> vector[]
+# -----------------------------------------------------------------------------
+
+
+def selectRandomCentroids(vectors, k, distance_fn):
+    import random
+    return random.sample(set(vectors), k)
+
+
+def selectSatisfactoryCentroids(vectors, k, distance_fn):
+    from hew.clusters.k_means import kmeans_plus_plus
+    return kmeans_plus_plus(vectors, k, distance_fn)
+
+# -----------------------------------------------------------------------------
+# vector -> Swatch
+# -----------------------------------------------------------------------------
+
+
+def rgbToSwatch(vector):
+    rgb = quantize(vector)
+    name = '#{0:02x}{1:02x}{2:02x}'.format(*rgb)
+    return Swatch(rgb, name)
+
+
+def hslToSwatch(vector):
+    rgb = toRgb(vector)
+    name = '#{0:02x}{1:02x}{2:02x}'.format(*rgb)
+    return Swatch(rgb, name)
+
+
+def useNearest(swatches, tree):
+    kvp = {sw.name: sw for sw in swatches}
+
+    def curry(vector):
+        _, name, _ = tree.nearest_neighbor(vector)
+        return kvp[name[0]]
+    return curry
+
+# -----------------------------------------------------------------------------
+# args -> string
+# -----------------------------------------------------------------------------
 
 
 def buildPathName(args):
     fileName = 'cluster-{0}'.format(args.k)
+
+    if args.random:
+        fileName += '-random'
 
     if args.hsl:
         fileName += '-hsl'
@@ -111,6 +82,9 @@ def buildPathName(args):
 def buildTitle(args):
     title = '{0} color palette'.format(args.k)
 
+    if args.random:
+        title += ' random start'
+
     if args.nearest:
         title += ' using original colors'
 
@@ -119,14 +93,95 @@ def buildTitle(args):
 
     return title
 
+# -----------------------------------------------------------------------------
+# Process
+# -----------------------------------------------------------------------------
+
+
+def run(args):
+    from operator import attrgetter
+    from display_clusters import Clusters
+    from hew.clusters.k_means import lloyds_algorithm
+    from hew.structures.kd_tree import KDTree
+    from hew.structures.vector import distance_euclid_squared
+    from hew.structures.vector import extractFloatVectors
+
+    # build a dictionary of swatches
+    swatches = sorted(Swatch.acquire(), key=attrgetter('saturation'))
+
+    # extract the vectors
+    if args.hsl:
+        valueFields = ['hue', 'saturation', 'light']
+    else:
+        valueFields = ['red', 'green', 'blue']
+    vectors = extractFloatVectors(swatches, valueFields)
+
+    if args.nearest:
+        print('Build nearest neighbor tree')
+        tree = KDTree.fromTable(swatches, valueFields, ['name'])
+
+    # determine the palette processing function
+    toPalette = None
+    if args.nearest:
+        toPalette = useNearest(swatches, tree)
+    elif args.hsl:
+        toPalette = hslToSwatch
+    else:
+        toPalette = rgbToSwatch
+
+    print('Select initial centroids')
+    distance_fn = distance_euclid_squared
+    if args.random:
+        MU0 = selectRandomCentroids(vectors, args.k, distance_fn)
+    else:
+        MU0 = selectSatisfactoryCentroids(vectors, args.k, distance_fn)
+
+    print('Find the clusters')
+    MU, clusterIndex, _ = lloyds_algorithm(vectors, MU0, distance_fn)
+
+    print('Process the centroids')
+    palette = list(map(toPalette, MU))
+
+    print('Write HTML')
+    pathName = buildPathName(args)
+    title = buildTitle(args)
+    page = Clusters(swatches, palette, clusterIndex)
+    page.render(pathName, title)
+
+# -----------------------------------------------------------------------------
+# Command Line
+# -----------------------------------------------------------------------------
+
+
+def buildArgParser():
+    import argparse
+
+    description = 'Demonstrate k-Means using Behr colors'
+    p = argparse.ArgumentParser(description=description)
+    p.add_argument('-k', '--clusters',
+                   type=int, dest='k', default=8,
+                   help='the number of clusters to create (default: 8)')
+    p.add_argument('-n', '--use-nearest',
+                   action='store_true', dest='nearest', default=False,
+                   help='cluster colors come from the input set')
+    p.add_argument('-s', '--show-steps',
+                   action='store_true', dest='steps', default=False,
+                   help='indicate the steps in the cluster calculation')
+    p.add_argument('-r', '--random',
+                   action='store_true', dest='random', default=False,
+                   help='start with random centroids')
+    p.add_argument('--use-hsl',
+                   action='store_true', dest='hsl', default=False,
+                   help='use HSL not RGB for cluster calc')
+    p.add_argument('--trial',
+                   metavar='NAME', dest='trial', required=False,
+                   help='identify a cluster run')
+
+    return p
+
 
 if __name__ == '__main__':
     parser = buildArgParser()
     args = parser.parse_args()
 
-    pathName = buildPathName(args)
-    title = buildTitle(args)
-
-    p = Clusters(**vars(args))
-    p.calculate()
-    p.render(pathName, title)
+    run(args)
---
+++ display_clusters.py
@@ -0,0 +1,19 @@
+from page import Page
+from operator import attrgetter
+
+
+class Clusters(Page):
+    def __init__(self, swatches, palette, indexes):
+        self.swatches = swatches
+        self.palette = palette
+        self.indexes = indexes
+
+    def onCanvas(self):
+        for i, mu in enumerate(self.indexes):
+            rgb = self.palette[mu].rgb
+            sw = self.swatches[i]
+            self.addClustered(rgb, sw)
+
+    def onLegend(self):
+        for sw in sorted(self.palette, key=attrgetter('hue', 'saturation')):
+            self.addLegend(sw.name, sw.rgb)
